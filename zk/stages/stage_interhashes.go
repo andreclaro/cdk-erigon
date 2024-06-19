478e7d2d52c90e34bdc48875093ee1785fdc9d77
André Claro <andre.claro@gmail.com>
2024-06-19 10:51:51 +0100

    chore: uncomment unwind loggic in stage_interhashes

--- zk/stages/stage_interhashes.go
+++ zk/stages/stage_interhashes.go
@@ -158,7 +158,8 @@ func SpawnZkIntermediateHashesStage(s *stagedsync.StageState, u stagedsync.Unwin
 
 		if root != expectedRootHash {
 			eridb.RollbackBatch()
-			panic(fmt.Sprintf("[%s] Wrong trie root of block %d: %x, expected (from header): %x. Block hash: %x", logPrefix, to, root, expectedRootHash, headerHash))
+			// panic(fmt.Sprintf("[%s] Wrong trie root of block %d: %x, expected (from header): %x. Block hash: %x", logPrefix, to, root, expectedRootHash, headerHash))
+			log.Error(fmt.Sprintf("[%s] Wrong trie root of block %d: %x, expected (from header): %x. Block hash: %x", logPrefix, to, root, expectedRootHash, headerHash))
 
 			if cfg.badBlockHalt {
 				return trie.EmptyRoot, fmt.Errorf("wrong trie root")
@@ -166,11 +167,11 @@ func SpawnZkIntermediateHashesStage(s *stagedsync.StageState, u stagedsync.Unwin
 			if cfg.hd != nil {
 				cfg.hd.ReportBadHeaderPoS(headerHash, syncHeadHeader.ParentHash)
 			}
-			// if to > s.BlockNumber {
-			//unwindTo := (to + s.BlockNumber) / 2 // Binary search for the correct block, biased to the lower numbers
-			//log.Warn("Unwinding due to incorrect root hash", "to", unwindTo)
-			//u.UnwindTo(unwindTo, headerHash)
-			// }
+			if to > s.BlockNumber {
+				unwindTo := (to + s.BlockNumber) / 2 // Binary search for the correct block, biased to the lower numbers
+				log.Warn("Unwinding due to incorrect root hash", "to", unwindTo)
+				u.UnwindTo(unwindTo, headerHash)
+			}
 		} else {
 			log.Info(fmt.Sprintf("[%s] State root matches", logPrefix))
 		}
